2c83db3e065e2ee31084d6042999300ce37e221f
BellaReal <101063556+BellaReal@users.noreply.github.com>
2022-04-26 16:20:21 -0700

    Making fake test

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,3 +1,9 @@
 public class SkillDemo {
-
+    public static boolean isEven(int e) {
+        if (e%2 == 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
